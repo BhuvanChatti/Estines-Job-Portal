f3be9baaf15aa2a80b3acfb9b23dcb56fd733566
BhuvanChatti <163702494+BhuvanChatti@users.noreply.github.com>
2025-03-28 08:24:26 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
         host:'smtp.gmail.com',
-        port:465,
-        secure: true,
+        port:587,
+        secure: false,
         user: 'bhuvanchatti579@gmail.com',
         pass: 'dfgp nbue aekc yizo'
     }
